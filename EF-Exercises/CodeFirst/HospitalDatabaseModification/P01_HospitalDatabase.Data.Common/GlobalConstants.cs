41f5e723be777d627f5eb0d68b313ae02a6dfb93
Yavor Gornalov <y.gornalov@gmail.com>
2024-06-01 16:26:08 +0300

    Hospital-Database-Modification Project --uploaded

---
+++ EF-Exercises/CodeFirst/HospitalDatabaseModification/P01_HospitalDatabase.Data.Common/GlobalConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace P01_HospitalDatabase.Data.Common;
+
+public static class GlobalConstants
+{
+    public const int NameMaxLength = 50;
+    public const int AddressMaxLength = 250;
+    public const int EmailMaxLength = 80;
+    public const int CommentMaxLength = 250;
+    public const int FullNameMaxLength = 100;
+    public const int SpecialityMaxLength = 100;
+}
---
+++ EF-Exercises/CodeFirst/HospitalDatabaseModification/P01_HospitalDatabase.Data.Models/Doctor.cs
@@ -0,0 +1,23 @@
+﻿using P01_HospitalDatabase.Data.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace P01_HospitalDatabase.Data.Models;
+
+public class Doctor
+{
+    public Doctor()
+    {
+        this.Visitations = new List<Visitation>();
+    }
+
+    [Key]
+    public int DoctorId { get; set; }
+
+    [Required, MaxLength(GlobalConstants.FullNameMaxLength)]
+    public string Name { get; set; } = null!;
+
+    [Required, MaxLength(GlobalConstants.SpecialityMaxLength)]
+    public string Specialty { get; set; } = null!;
+
+    public virtual ICollection<Visitation> Visitations { get; set; }
+}
